2f5090950fd4f22f40e089fad2c6f2f9ebbba561
Subham Sharma <sharma.subham.96@gmail.com>
2017-02-01 18:42:48 +0530

    initial commit

---
+++ track.py
@@ -0,0 +1,60 @@
+#developed by subham sharma
+from easygui import *
+import sys
+import urllib
+import socket
+import pygeoip
+import cStringIO 
+from PIL import Image
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+
+def get_static_google_map(center=None, zoom=None, imgsize=None, maptype="roadmap", markers=None ):  
+    """retrieve a map (image) from the static google maps server
+         Creates a request string with a URL like this:
+        http://maps.google.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=14&size=512x512&maptype=roadmap
+&markers=color:blue|label:S|40.702147,-74.015794&sensor=false"""
+   
+    
+    # assemble the URL
+    request =  "http://maps.google.com/maps/api/staticmap?" 
+   
+    # if center and zoom  are not given, the map will show all marker locations
+    if center != None:
+        request += "center=%s&" % center
+    if center != None:
+        request += "zoom=%i&" % zoom 
+
+
+    request += "size=%ix%i&" % (imgsize) 
+    request += "format=png&" 
+    request += "maptype=%s&" % maptype  
+
+    if markers != None:
+       request += "markers=%s&" % markers
+
+    request += "sensor=false&"   
+    
+ 
+    web_sock = urllib.urlopen(request)
+    imgdata = cStringIO.StringIO(web_sock.read()) 
+    try:
+        PIL_img = Image.open(imgdata)
+    
+    except IOError:
+        print "IOError:", imgdata.read()
+     
+    else:
+        PIL_img.save("gmap.png", "PNG") # save as png image in disk
+        image_name="gmap.png"
+        return image_name
+    addr='
+    country=gi.country_name_by_addr(addr)
+    l=gi.record_by_addr(addr)
+    centerlocation=str(l["latitude"])+","+str(l['longitude'])
+    image_name=get_static_google_map(center=centerlocation, zoom=15, imgsize=(480,320),maptype="terrain", markers="size:mid|label:B|color:red|"+centerlocation+"|" )
+    try:
+        msgbox(msg='Domain Name :-  '+  hostname + '\nIP Address:-  '+ addr + '\nCountry:-  ' + country + '\nCity:-  '+ l['city'] +'\nLatitude:-  '+ str(l['latitude']) +'\nLongitude:-  '+ str(l['longitude']) , title='IP Finder', ok_button='OK', image=image_name)
+    except:
+        msgbox(msg='SORRY DOMAIN NOT REGISTERED IN DATABASE', title='IP Finder', ok_button='OK', image="error.gif")
